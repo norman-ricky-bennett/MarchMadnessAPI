eace45fe3425697887103be0fc72879c5bf40705
Ricky Bennett <norman.bennett525@gmail.com>
2021-03-08 10:24:31 -0800

    3/8/21

---
+++ MarchMadness/MarchMadnessAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MarchMadnessAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ MarchMadness/MarchMadnessAPI/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(MarchMadnessAPI.Startup))]
+
+namespace MarchMadnessAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
