38ed67d30eedfb51da745840da616d6a069249c4
YatzuriR <yatzuri.rojas@gmail.com>
2021-03-10 14:20:56 -0500

    players

---
+++ MarchMadness/MarchMadness.Models/PlayerDetail.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarchMadness.Models
+{
+    public class PlayerDetail
+    {
+        public int PlayerId { get; set; }
+        
+        public string Name { get; set; }
+        
+        public string Position { get; set; }
+
+        public int SeasonRebounds { get; set; }
+
+        public int SeasonAssists { get; set; }
+        
+        public int SeasonTotalPoints { get; set; }
+
+    }
+}
---
+++ MarchMadness/MarchMadness.Models/PlayerEdit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarchMadness.Models
+{
+    public class PlayerEdit
+    {
+        public int PlayerId { get; set; }
+        public int SeasonRebounds { get; set; }
+        public int SeasonAssists { get; set; }
+        public int SeasonTotalPoints { get; set; }
+    }
+}
